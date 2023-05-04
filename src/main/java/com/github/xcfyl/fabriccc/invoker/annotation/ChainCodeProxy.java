aa6a242016c7ab88bc8b904ad85ea6e17ff96a64
西城风雨楼 <aiker67@163.com>
2023-05-04 14:27:39 +0800

    去除了ChainCodeProxy注解中的Component注解

--- src/main/java/com/github/xcfyl/fabriccc/invoker/annotation/ChainCodeProxy.java
+++ src/main/java/com/github/xcfyl/fabriccc/invoker/annotation/ChainCodeProxy.java
@@ -12,7 +12,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Component
 public @interface ChainCodeProxy {
     String channelName();
 }
